71b3bf8e6e41790d6b778b19e3073b2dbba70922
Guust-Franssens <89594420+Guust-Franssens@users.noreply.github.com>
2025-01-07 19:07:55 +0100

    revert setting change

--- src/settings.py
+++ src/settings.py
@@ -23,7 +23,7 @@ NEWSPIDER_MODULE = "src.spiders"
 # USER_AGENT = "... (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32
