0e81628bfdfc5fbbf1b5ececb1e1f955d6be01ee
Guust Franssens <89594420+Guust-Franssens@users.noreply.github.com>
2025-09-04 13:32:12 +0200

    add key vault as parameter in variable library

--- fabric/NB_publish_to_huggingface.Notebook/notebook-content.py
+++ fabric/NB_publish_to_huggingface.Notebook/notebook-content.py
@@ -35,11 +35,22 @@ from pyspark.sql.functions import col
 # META   "language_group": "synapse_pyspark"
 # META }
 
+# PARAMETERS CELL ********************
+
+table_name = "belgian_journal"
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
 # CELL ********************
 
 silver_lakehouse = notebookutils.lakehouse.get("LH_silver").properties["abfsPath"]
-table_name = "belgian-journal"
 silver_table_path = f"{silver_lakehouse}/Tables/{table_name}"
+variables = notebookutils.variableLibrary.getLibrary("VL_environment_variables")
 
 # METADATA ********************
 
@@ -50,29 +61,8 @@ silver_table_path = f"{silver_lakehouse}/Tables/{table_name}"
 
 # CELL ********************
 
-df = spark.read.format("delta").load(silver_table_path)
-
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
-
-df2 = spark.read.format("delta").load("abfss://30953eab-1865-42f8-a9f5-9c644079f1c3@onelake.dfs.fabric.microsoft.com/9f317ee6-48b0-4c36-b84e-100c370be617/Tables/belgian-journal")
-
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
-
-df_all = df.union(df2).dropDuplicates()
+silver_table_path = "abfss://f259d7ab-0ec3-4190-b094-4cde9963712f@onelake.dfs.fabric.microsoft.com/1f4fd51b-d57a-44f4-b1f3-e34a6c7bef7c/Tables/belgian_journal"
+df = spark.read.format("delta").load(silver_table_path).dropDuplicates()
 
 # METADATA ********************
 
@@ -84,7 +74,7 @@ df_all = df.union(df2).dropDuplicates()
 # CELL ********************
 
 spark.conf.set("spark.sql.files.maxPartitionBytes", "524288000") 
-df_all.filter(col("publication_date") < date(2025, 6, 1)).repartition(20).write.format("parquet").save(silver_lakehouse + f"/Files/{table_name}")
+df.filter(col("publication_date") < date(2025, 9, 1)).repartition(20).write.format("parquet").save(silver_lakehouse + f"/Files/{table_name}")
 notebookutils.fs.rm(silver_lakehouse + f"/Files/{table_name}/_SUCCESS")
 
 # METADATA ********************
@@ -96,7 +86,7 @@ notebookutils.fs.rm(silver_lakehouse + f"/Files/{table_name}/_SUCCESS")
 
 # CELL ********************
 
-token = notebookutils.credentials.getSecret("https://kv-fabric-belux.vault.azure.net/", "token-huggingface")
+token = notebookutils.credentials.getSecret(variables.key_vault_url, "token-huggingface")
 login(token)
 api = HfApi()
 
@@ -111,10 +101,10 @@ api = HfApi()
 
 api.upload_folder(
     repo_id="guust-franssens/belgian-journal",
-    folder_path="/lakehouse/default/Files/belgian-journal",
+    folder_path=f"/lakehouse/default/Files/{table_name}",
     path_in_repo="data",
     repo_type="dataset",
-    commit_message="add march april may",
+    commit_message="add June July August",
     create_pr=True,
     delete_patterns="*.parquet",
 )
